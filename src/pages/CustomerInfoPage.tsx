d36dfe44992d1727cd02904e6f340961e5b8bd51
SergejShk <sshkurupij11@gmail.com>
2022-11-27 04:51:02 +0200

    added Customer Info mob

--- src/pages/CustomerInfoPage.tsx
+++ src/pages/CustomerInfoPage.tsx
@@ -1,5 +1,37 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import BtnGoBack from "../components/shared/BtnGoBack";
+import DataInfoList from "../components/shared/DataInfoList";
+import DataTitle from "../components/shared/DataTitle";
+import { getDataInfoById } from "../services/dataApi";
+
 const CustomerInfoPage: React.FC = () => {
-  return <p>This is customer</p>;
+  const { customerId } = useParams();
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    getDataInfoById({
+      category: "customers",
+      id: customerId!,
+    })
+      .then((res) => {
+        setData(res);
+      })
+      .catch(console.log);
+  }, [customerId]);
+
+  if (!data) return <p>No such customer</p>;
+  return (
+    <>
+      {Object.keys(data).length > 0 && (
+        <>
+          <DataTitle isIconInfo>Customer information</DataTitle>
+          <DataInfoList data={data} />
+          <BtnGoBack />
+        </>
+      )}
+    </>
+  );
 };
 
 export default CustomerInfoPage;
